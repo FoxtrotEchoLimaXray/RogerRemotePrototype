f6d3f8246e43d811dbda61b64d356e14a849fd06
FoxtrotEchoHotel <143741439+FoxtrotEchoHotel@users.noreply.github.com>
2023-09-15 13:41:15 +0200

    Add files via upload

---
+++ FindMy/FindMyRoger/FindMyRogerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FindMyRogerApp.swift
+//  FindMyRoger
+//
+//  Created by El-Hoiydi, Felix on 13.09.23.
+//
+
+import SwiftUI
+
+@main
+struct FindMyRogerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
